5aaa7da177d9c82ca9f9d3023ab382d763169057
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2025-03-08 21:34:32 +0530

    refactor: sperate listener files

---
+++ services/events/src/listeners/onOrbitSphereInstanceRented.ts
@@ -0,0 +1,45 @@
+/** @notice Library imports */
+import type { CreateRentalLogParams } from "@orbitsphere/database/schemas";
+/// Local imports
+import { orbitsphere, orbitSphereEventsHandler } from "@/configs/clients";
+
+await orbitsphere.onOrbitSphereInstanceRented(
+  async (
+    region,
+    sphereId,
+    instanceType,
+    sshPublicKey,
+    rentedOn,
+    willBeEndOn,
+    tenant,
+    totalCost,
+    payload
+  ) => {
+    try {
+      /// Adding into Rental queue
+
+      /// Recoding `OrbitSphereInstanceRented` into database
+      const { blockNumber, transactionHash, data, topics } = payload.log;
+      await orbitSphereEventsHandler.recordRentalLog({
+        /// Core
+        region,
+        sphereId,
+        instanceType,
+        sshPublicKey,
+        rentedOn,
+        willBeEndOn,
+        tenant,
+        totalCost,
+        /// Blockchain
+        data,
+        topics,
+        transactionHash,
+        blockNumber: BigInt(blockNumber),
+      } satisfies CreateRentalLogParams);
+
+      console.log(`Processing sphere id ${sphereId.toString()}...`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
