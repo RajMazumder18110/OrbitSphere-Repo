85f11ac16fb72f130f587f0875611b676db74c98
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2025-03-09 01:57:43 +0530

    feat: wait for deployment

--- packages/aws/src/index.ts
+++ packages/aws/src/index.ts
@@ -4,7 +4,9 @@ import {
   _InstanceType,
   RunInstancesCommand,
   StopInstancesCommand,
+  DescribeInstancesCommand,
   TerminateInstancesCommand,
+  type Instance,
   type RunInstancesCommandInput,
   type StopInstancesCommandInput,
   type TerminateInstancesCommandInput,
@@ -20,29 +22,72 @@ import type { LaunchInstanceParams, RunningInstanceParams } from "@/types";
 class OrbitSphereAWSInstance {
   constructor(private accessKeyId: string, private secretAccessKey: string) {}
 
-  public async launch(data: LaunchInstanceParams) {
-    /// Initializing instance
-    const client = new EC2Client({
-      region: data.region,
-      credentials: {
-        accessKeyId: this.accessKeyId,
-        secretAccessKey: this.secretAccessKey,
-      },
-    });
-    /// Preparing data
-    const params: RunInstancesCommandInput = {
-      MinCount: 1,
-      MaxCount: 1,
-      ImageId: AWSImageIds.UBUNTU,
-      SecurityGroupIds: [AWSSecurityGroups.EXPOSE_EVERYTHING],
-      UserData: Buffer.from(
-        getCustomSSHKeySetupCommand(data.sshPublicKey)
-      ).toString("base64"),
-      InstanceType: data.instanceType as _InstanceType,
-    };
+  private async wait(client: EC2Client, instanceId: string): Promise<Instance> {
+    return new Promise(async (resolve, reject) => {
+      while (true) {
+        const describeCommand = new DescribeInstancesCommand({
+          InstanceIds: [instanceId],
+        });
 
-    /// Running instance
-    return await client.send(new RunInstancesCommand(params));
+        try {
+          const describeResponse = await client.send(describeCommand);
+          const instance =
+            describeResponse.Reservations?.at(0)?.Instances?.at(0);
+          const publicIp = instance?.PublicIpAddress;
+
+          if (publicIp) {
+            return resolve(instance);
+          }
+        } catch (error) {
+          return reject(error);
+        }
+
+        // Wait 10 seconds before retrying
+        await new Promise((res) => setTimeout(res, 10000));
+      }
+    });
+  }
+
+  public async launch(data: LaunchInstanceParams): Promise<Instance> {
+    return new Promise((resolve, reject) => {
+      console.log(data);
+
+      /// Initializing instance
+      const client = new EC2Client({
+        region: data.region,
+        credentials: {
+          accessKeyId: this.accessKeyId,
+          secretAccessKey: this.secretAccessKey,
+        },
+      });
+
+      /// Preparing data
+      const params: RunInstancesCommandInput = {
+        MinCount: 1,
+        MaxCount: 1,
+        ImageId: AWSImageIds.UBUNTU,
+        SecurityGroupIds: [AWSSecurityGroups.EXPOSE_EVERYTHING],
+        UserData: Buffer.from(
+          getCustomSSHKeySetupCommand(data.sshPublicKey)
+        ).toString("base64"),
+        InstanceType: data.instanceType as _InstanceType,
+      };
+
+      /// Running instance
+      client
+        .send(new RunInstancesCommand(params))
+        .then((response) => {
+          const instanceId = response.Instances?.at(0)?.InstanceId!;
+          if (!instanceId) reject(new Error("Unable to run instance!"));
+          /// Wait for deployment.
+          return this.wait(client, instanceId);
+        })
+        .then((instance) => {
+          console.log(`Deployed ip`, instance.PublicIpAddress);
+          resolve(instance);
+        })
+        .catch((err) => reject(err));
+    });
   }
 
   public async terminate(data: RunningInstanceParams) {
