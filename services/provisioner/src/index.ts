ff7d9d896ee56a59a2c7694fe093acaeb8c3c65f
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2025-03-09 10:04:01 +0530

    feat: Add instance details into database after running

--- services/provisioner/src/index.ts
+++ services/provisioner/src/index.ts
@@ -1,12 +1,26 @@
-/** @notice Local imports */
-import { orbitsphereRentalQueue, orbitsphereAWS } from "@/configs/clients";
+/** @notice Library imports */
+import type { CreateInstanceParams } from "@orbitsphere/database/schemas";
+/// lOcal imports
+import {
+  orbitsphereAWS,
+  orbitSphereDatabase,
+  orbitsphereRentalQueue,
+} from "@/configs/clients";
 
 await orbitsphereRentalQueue.consume(async (payload) => {
   /// Creating AWS instance
   const instance = await orbitsphereAWS.launch(payload);
-  console.log({
-    aws: instance.PublicIpAddress,
-  });
+  const { PublicIpAddress, PrivateIpAddress, InstanceId } = instance;
+  /// Saving instance details into database
+  await orbitSphereDatabase.instances.create({
+    region: payload.region,
+    instanceId: InstanceId!,
+    instanceType: payload.instanceType,
+    sphereId: BigInt(payload.sphereId),
+    publicIp: PublicIpAddress!,
+    privateIp: PrivateIpAddress!,
+    tenant: payload.tenant,
+  } satisfies CreateInstanceParams);
 });
 
 console.log("Consuming...");
