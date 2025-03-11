7d5780cb3f398ff70f0855eb26e8fc35c79e3c0b
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2025-03-11 23:17:35 +0530

    feat: Add connect dialog and termination status

--- frontend/src/app/dashboard/[instanceId]/page.tsx
+++ frontend/src/app/dashboard/[instanceId]/page.tsx
@@ -1,5 +1,7 @@
 /** @notice library imports */
+import Link from "next/link";
 import { FaMemory } from "react-icons/fa";
+import { IoGitNetwork } from "react-icons/io5";
 import { TbClockPlay } from "react-icons/tb";
 import { BsFillCpuFill } from "react-icons/bs";
 import { IoLogoUsd, IoMdGlobe } from "react-icons/io";
@@ -13,20 +15,31 @@ import { Badge } from "@/components/ui/badge";
 import {
   Card,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import RadialChart from "./RefundChart";
 import UtilizationChart from "./UtilizationChart";
 import {
+  calculateRefundAmount,
   capitalize,
   getCompletionPercentage,
   getDegree,
   getTimeRemaining,
+  isInstanceTerminated,
 } from "@/lib/utils";
 import { getInstancesById, getUtilizationData } from "@/actions";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import CopyButton from "@/components/CopyButton";
 
 /// Type
 interface SingleInstanceDetailsProps {
@@ -70,135 +83,235 @@ const SingleInstanceDetails = async ({
     );
   }
 
+  /// Monitoring data
+  const utilizationData = await getUtilizationData();
   /// Calculating data
-  const isTerminated = instance.status === "TERMINATED";
+  const isTerminated = isInstanceTerminated(instance);
+  const refundAmount = calculateRefundAmount(instance);
   const completedPercentage = getCompletionPercentage(
     instance.rentedOn,
     instance.willBeEndOn
   );
 
-  const refundAmount = 1.5; // USDC
-
-  const utilizationData = await getUtilizationData();
-
   return (
-    <div className="w-full flex flex-col gap-10">
-      <div className="flex flex-col gap-7">
-        <Breadcrumbs />
-        <header className="flex gap-3 items-center">
-          <h1 className="text-3xl font-semibold font-[family-name:var(--font-geist-mono)]">
-            &gt; {instanceId}
-          </h1>
-          <MdOutlineRadioButtonChecked
-            className={`${
-              isTerminated ? "text-red-500" : "text-green-500"
-            } text-lg`}
-          />
-        </header>
+    <Dialog>
+      <div className="w-full flex flex-col gap-10">
+        <div className="flex flex-col gap-7">
+          <Breadcrumbs />
+          <header className="flex justify-between gap-3 items-center">
+            <div className="flex gap-3 items-center">
+              <h1 className="text-3xl font-semibold font-[family-name:var(--font-geist-mono)]">
+                &gt; {instanceId}
+              </h1>
+              <MdOutlineRadioButtonChecked
+                className={`${
+                  isTerminated ? "text-red-500" : "text-green-500"
+                } text-lg`}
+              />
+            </div>
+            {!isTerminated && (
+              <DialogTrigger asChild>
+                <Button
+                  variant="secondary"
+                  type="button"
+                  className="cursor-pointer"
+                >
+                  Connect
+                </Button>
+              </DialogTrigger>
+            )}
+          </header>
 
-        <section className="w-full flex flex-col gap-5">
-          <div className="flex items-center gap-3">
-            <LuCalendarClock className="text-2xl" />
-            <h1>{instance.willBeEndOn.toString()}</h1>
-          </div>
-          <div className="flex items-center gap-3">
-            <Badge className="text-xs font-semibold dark rounded-full">
-              {completedPercentage}%
-            </Badge>
-            <Progress value={completedPercentage} className="w-full h-4 dark" />
-          </div>
+          <section className="w-full flex flex-col gap-5">
+            <div className="flex items-center gap-3">
+              <LuCalendarClock className="text-2xl" />
+              <h1>{instance.willBeEndOn.toString()}</h1>
+            </div>
+            <div className="flex items-center gap-3">
+              <Badge className="text-xs font-semibold dark rounded-full">
+                {completedPercentage}%
+              </Badge>
+              <Progress
+                value={completedPercentage}
+                className="w-full h-4 dark"
+              />
+            </div>
+          </section>
+        </div>
+
+        <section className="dark grid grid-cols-2 md:grid-cols-4 gap-4 text-center font-[family-name:var(--font-geist-mono)]">
+          <Card className="gap-1">
+            <CardHeader className="flex flex-col items-center gap-3">
+              <MdOutlineRadioButtonChecked className="text-2xl" />
+              <div className="flex flex-col gap-1 items-center">
+                <CardTitle>Status</CardTitle>
+                <CardDescription>
+                  {isTerminated ? "Terminated" : "Running"}
+                </CardDescription>
+              </div>
+            </CardHeader>
+          </Card>
+          <Card className="gap-1">
+            <CardHeader className="flex flex-col items-center gap-3">
+              <IoLogoUsd className="text-2xl" />
+              <div className="flex flex-col gap-1 items-center">
+                <CardTitle>Consumed</CardTitle>
+                <CardDescription>
+                  {(instance.totalCost - refundAmount).toFixed(2)} USDC
+                </CardDescription>
+              </div>
+            </CardHeader>
+          </Card>
+          <Card className="gap-1">
+            <CardHeader className="flex flex-col items-center gap-3">
+              <TbClockPlay className="text-2xl" />
+              <div className="flex flex-col gap-1 items-center">
+                <CardTitle>Remaining</CardTitle>
+                <CardDescription>
+                  {getTimeRemaining(instance.willBeEndOn)}
+                </CardDescription>
+              </div>
+            </CardHeader>
+          </Card>
+          <Card className="gap-1">
+            <CardHeader className="flex flex-col items-center gap-3">
+              <FaLocationCrosshairs className="text-2xl" />
+              <div className="flex flex-col gap-1 items-center">
+                <CardTitle>Region</CardTitle>
+                <CardDescription>
+                  {capitalize(instance.region.name)}
+                </CardDescription>
+              </div>
+            </CardHeader>
+          </Card>
+          <Card className="gap-1">
+            <CardHeader className="flex flex-col items-center gap-3">
+              <IoMdGlobe className="text-2xl" />
+              <div className="flex flex-col gap-1 items-center">
+                <CardTitle>Sphere</CardTitle>
+                <CardDescription>{instance.id}</CardDescription>
+              </div>
+            </CardHeader>
+          </Card>
+          <Card className="gap-1">
+            <CardHeader className="flex flex-col items-center gap-3">
+              <LuServer className="text-2xl" />
+              <div className="flex flex-col gap-1 items-center">
+                <CardTitle>Type</CardTitle>
+                <CardDescription>{instance.instanceType}</CardDescription>
+              </div>
+            </CardHeader>
+          </Card>
+          <Card className="gap-1">
+            <CardHeader className="flex flex-col items-center gap-3">
+              <BsFillCpuFill className="text-2xl" />
+              <div className="flex flex-col gap-1 items-center">
+                <CardTitle>vCPUs</CardTitle>
+                <CardDescription>{instance.sphere.noOfCPUs}</CardDescription>
+              </div>
+            </CardHeader>
+          </Card>
+          <Card className="gap-1">
+            <CardHeader className="flex flex-col items-center gap-2">
+              <FaMemory className="text-2xl" />
+              <div className="flex flex-col gap-1 items-center">
+                <CardTitle>Memory</CardTitle>
+                <CardDescription>
+                  {instance.sphere.memoryGBs} GiB
+                </CardDescription>
+              </div>
+            </CardHeader>
+          </Card>
+        </section>
+
+        <section className="dark grid grid-cols-1 sm:grid-cols-3 gap-5 text-center">
+          <RadialChart
+            value={refundAmount}
+            circleFillDeg={getDegree(refundAmount, instance.totalCost)}
+          />
+          <UtilizationChart data={utilizationData} />
         </section>
       </div>
 
-      <section className="dark grid grid-cols-2 md:grid-cols-4 gap-4 text-center font-[family-name:var(--font-geist-mono)]">
-        <Card className="gap-1">
-          <CardHeader className="flex flex-col items-center gap-3">
-            <MdOutlineRadioButtonChecked className="text-2xl" />
-            <div className="flex flex-col gap-1 items-center">
-              <CardTitle>Status</CardTitle>
-              <CardDescription>{capitalize(instance.status)}</CardDescription>
-            </div>
-          </CardHeader>
-        </Card>
-        <Card className="gap-1">
-          <CardHeader className="flex flex-col items-center gap-3">
-            <IoLogoUsd className="text-2xl" />
-            <div className="flex flex-col gap-1 items-center">
-              <CardTitle>Consumed</CardTitle>
-              <CardDescription>
-                {instance.totalCost - refundAmount} USDC
-              </CardDescription>
-            </div>
-          </CardHeader>
-        </Card>
-        <Card className="gap-1">
-          <CardHeader className="flex flex-col items-center gap-3">
-            <TbClockPlay className="text-2xl" />
-            <div className="flex flex-col gap-1 items-center">
-              <CardTitle>Remaining</CardTitle>
-              <CardDescription>
-                {getTimeRemaining(instance.willBeEndOn)}
-              </CardDescription>
-            </div>
-          </CardHeader>
-        </Card>
-        <Card className="gap-1">
-          <CardHeader className="flex flex-col items-center gap-3">
-            <FaLocationCrosshairs className="text-2xl" />
-            <div className="flex flex-col gap-1 items-center">
-              <CardTitle>Region</CardTitle>
-              <CardDescription>
-                {capitalize(instance.region.name)}
-              </CardDescription>
-            </div>
-          </CardHeader>
-        </Card>
-        <Card className="gap-1">
-          <CardHeader className="flex flex-col items-center gap-3">
-            <IoMdGlobe className="text-2xl" />
-            <div className="flex flex-col gap-1 items-center">
-              <CardTitle>Sphere</CardTitle>
-              <CardDescription>{instance.id}</CardDescription>
-            </div>
-          </CardHeader>
-        </Card>
-        <Card className="gap-1">
-          <CardHeader className="flex flex-col items-center gap-3">
-            <LuServer className="text-2xl" />
-            <div className="flex flex-col gap-1 items-center">
-              <CardTitle>Type</CardTitle>
-              <CardDescription>{instance.instanceType}</CardDescription>
-            </div>
-          </CardHeader>
-        </Card>
-        <Card className="gap-1">
-          <CardHeader className="flex flex-col items-center gap-3">
-            <BsFillCpuFill className="text-2xl" />
-            <div className="flex flex-col gap-1 items-center">
-              <CardTitle>vCPUs</CardTitle>
-              <CardDescription>{instance.sphere.noOfCPUs}</CardDescription>
-            </div>
-          </CardHeader>
-        </Card>
-        <Card className="gap-1">
-          <CardHeader className="flex flex-col items-center gap-2">
-            <FaMemory className="text-2xl" />
-            <div className="flex flex-col gap-1 items-center">
-              <CardTitle>Memory</CardTitle>
-              <CardDescription>{instance.sphere.memoryGBs} GiB</CardDescription>
-            </div>
-          </CardHeader>
-        </Card>
-      </section>
+      {/* Connect Dialog */}
+      {!isTerminated && (
+        <DialogContent className="dark border-gray-800 flex flex-col gap-7">
+          <DialogHeader>
+            <DialogTitle className="text-2xl font-semibold font-[family-name:var(--font-geist-mono)]">
+              Connect your sphere
+            </DialogTitle>
+            <DialogDescription>
+              Follow the steps below to connect securely to your rented server.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="w-full grid grid-cols-2 gap-5">
+            {/* Connection ip */}
+            <Card className="gap-1">
+              <CardHeader className="flex flex-col items-center gap-2">
+                <IoGitNetwork className="text-2xl" />
+                <div className="flex flex-col gap-1 items-center">
+                  <CardTitle className="text-muted-foreground font-semibold">
+                    Public IP
+                  </CardTitle>
+                  <CardDescription className="text-gray-200 font-bold text-lg font-[family-name:var(--font-geist-mono)]">
+                    {instance.publicIp}
+                  </CardDescription>
+                </div>
+              </CardHeader>
+              <CardFooter className="w-full flex items-center justify-center">
+                <CopyButton text={instance.publicIp} />
+              </CardFooter>
+            </Card>
 
-      <section className="dark grid grid-cols-1 sm:grid-cols-3 gap-5 text-center">
-        <RadialChart
-          value={refundAmount}
-          circleFillDeg={getDegree(refundAmount, instance.totalCost)}
-        />
-        <UtilizationChart data={utilizationData} />
-      </section>
-    </div>
+            <Card className="gap-1">
+              <CardHeader className="flex flex-col items-center gap-2">
+                <IoGitNetwork className="text-2xl" />
+                <div className="flex flex-col gap-1 items-center">
+                  <CardTitle className="text-muted-foreground font-semibold">
+                    Private IP
+                  </CardTitle>
+                  <CardDescription className="text-gray-200 font-bold text-lg font-[family-name:var(--font-geist-mono)]">
+                    {instance.privateIp}
+                  </CardDescription>
+                </div>
+              </CardHeader>
+              <CardFooter className="w-full flex items-center justify-center">
+                <CopyButton text={instance.privateIp} />
+              </CardFooter>
+            </Card>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h1 className="text-muted-foreground">
+              Protect your private key by running this command.
+            </h1>
+            <Card>
+              <CardHeader>
+                <CardDescription className="flex items-center justify-between text-gray-200 p-0">
+                  <code className="font-[family-name:var(--font-geist-mono)]">{`chmod 400 your-key.pem`}</code>
+                  <CopyButton text={`chmod 400 your-key.pem`} />
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <h1 className="text-muted-foreground">
+              Use SSH to connect to your sphere.
+            </h1>
+            <Card>
+              <CardHeader>
+                <CardDescription className="flex items-center justify-between text-gray-200 p-0">
+                  <code className="font-[family-name:var(--font-geist-mono)]">{`ssh -i your-key.pem ubuntu@${instance.publicIp}`}</code>
+                  <CopyButton
+                    text={`ssh -i your-key.pem ubuntu@${instance.publicIp}`}
+                  />
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </div>
+        </DialogContent>
+      )}
+    </Dialog>
   );
 };
 
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,10 +1,34 @@
 import { twMerge } from "tailwind-merge";
 import { padHex, stringToHex } from "viem";
 import { clsx, type ClassValue } from "clsx";
+import { Instance } from "@/actions";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const calculateRefundAmount = (instance: Instance): number => {
+  const currentTime = new Date();
+  const totalDuration =
+    (instance.willBeEndOn.getTime() - instance.rentedOn.getTime()) / 3600000; // Total hours
+  const elapsedTime =
+    (currentTime.getTime() - instance.rentedOn.getTime()) / 3600000; // Hours used
+
+  if (elapsedTime >= totalDuration) return 0; // No refund if fully used
+  if (elapsedTime <= 0) return instance.totalCost; // Full refund if not started
+
+  const usedCost = elapsedTime * instance.sphere.hourlyRate;
+  const refundAmount = instance.totalCost - usedCost;
+
+  return Math.max(Number(refundAmount.toFixed(2)), 0); // Ensure refund is not negative
+};
+
+export const isInstanceTerminated = (instance: Instance): boolean => {
+  const currentTime = new Date();
+  return (
+    instance.status === "TERMINATED" || currentTime >= instance.willBeEndOn
+  );
+};
 
 export const getTimeRemaining = (willBeEndOn: Date): string => {
   const currentTime = new Date();
