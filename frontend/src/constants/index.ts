b314415d454f7026c236d71080d4d61122272d15
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2025-03-14 12:14:49 +0530

    fix: client side env

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,16 +1,15 @@
 import { Hex } from "viem";
 /// Environements ///
 
-/// Blockchain
-export const USDC = process.env.USDC as Hex;
-export const ORBITSPHERE = process.env.ORBIT_SPHERE_CONTRACT as Hex;
-
-/// Appliaction constants
-export const DATABASE_CONNECTION_URL = process.env.DATABASE_CONNECTION_URL;
+/// Client variables
+export const USDC = process.env.NEXT_PUBLIC_USDC as Hex;
+export const ORBITSPHERE = process.env.NEXT_PUBLIC_ORBIT_SPHERE_CONTRACT as Hex;
 export const MINIMUM_TIME_TO_RENT =
-  Number(process.env.MINIMUM_TIME_TO_RENT) ?? 10;
-export const DEFAULT_PAGE_LIMIT = Number(process.env.DEFAULT_PAGE_LIMIT) ?? 8;
+  Number(process.env.NEXT_PUBLIC_MINIMUM_TIME_TO_RENT) ?? 10;
+export const DEFAULT_PAGE_LIMIT =
+  Number(process.env.NEXT_PUBLIC_DEFAULT_PAGE_LIMIT) ?? 8;
 
-/// AWS
+/// Server variables
+export const DATABASE_CONNECTION_URL = process.env.DATABASE_CONNECTION_URL;
 export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
